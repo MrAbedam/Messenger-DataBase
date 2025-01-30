e7b81f302d2dc1db669711a1713ab07ab4bcab73
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2025-01-30 21:43:32 +0330

    add Message tables

--- rads_part.sql
+++ rads_part.sql
@@ -1,1 +1,60 @@
 -- abeds_part
+
+CREATE TABLE Message
+(
+    message_id     UUID PRIMARY KEY                     DEFAULT gen_random_uuid(),
+    send_time      TIMESTAMP                            DEFAULT now(),
+    message_type   STRING(10) NOT NULL CHECK (message_type IN ('normal', 'poll', 'sticker')),
+    sender_user_id UUID NOT NULL REFERENCES Users (user_id),
+    conv_id        UUID NOT NULL REFERENCES Conversation (conv_id),
+    forward_from   UUID REFERENCES Message (message_id) DEFAULT null,
+    reply_to       UUID REFERENCES Message (message_id) DEFAULT null
+);
+
+CREATE TABLE Normal_Message
+(
+    message_id UUID PRIMARY KEY REFERENCES Message (message_id),
+    edit_time  TIMESTAMP,
+    text       STRING(500)
+);
+
+CREATE TABLE Message_Sticker
+(
+    message_id UUID PRIMARY KEY REFERENCES Message (message_id),
+    sticker_id UUID NOT NULL REFERENCES Sticker (sticker_id)
+);
+
+CREATE TABLE Poll_Message
+(
+    message_id     UUID PRIMARY KEY REFERENCES Message (message_id),
+    question       STRING(100) NOT NULL,
+    poll_type      BIT(3) NOT NULL DEFAULT B'100',
+    poll_option_id UUID            DEFAULT NULL
+);
+
+CREATE TABLE Poll_Option
+(
+    opt_id       UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    message_id   UUID             NOT NULL REFERENCES Poll_Message (message_id),
+    option_text  STRING(50),
+    vote_counter INT                       DEFAULT 0
+);
+
+ALTER TABLE Poll_Message
+    ADD CONSTRAINT fk_poll_option_id FOREIGN KEY (poll_option_id) REFERENCES Poll_Option (opt_id);
+
+CREATE TABLE Vote
+(
+    user_id        UUID NOT NULL REFERENCES Human_user (user_id),
+    poll_option_id UUID NOT NULL REFERENCES Poll_Option (opt_id),
+    PRIMARY KEY (user_id, poll_option_id)
+);
+
+CREATE TABLE Reacts (
+                        user_id UUID NOT NULL REFERENCES Human_user(user_id),
+                        message_id UUID NOT NULL REFERENCES Message(message_id),
+                        emoji STRING(10) DEFAULT '👍🏻' ,
+                        time TIMESTAMP DEFAULT now(),
+                        PRIMARY KEY (user_id, message_id)
+);
+
