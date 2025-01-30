bf2184e4fff659ac92c371ee96086672922f0e1d
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2025-01-30 18:36:48 +0330

    add all Users Tables

--- rads_part.sql
+++ rads_part.sql
@@ -59,4 +59,68 @@ CREATE TABLE Contact (
                          last_name STRING(10) DEFAULT '',
                          PRIMARY KEY (user_id, c_user_id)
 );
+CREATE TABLE Premium (
+                         user_id UUID PRIMARY KEY REFERENCES Human_user(user_id),
+                         expiration_date DATE DEFAULT now()
+);
+
+CREATE TABLE Private_Prof (
+                              media_id UUID PRIMARY KEY,
+                              user_id UUID NOT NULL REFERENCES Human_user(user_id),
+                              time_set TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE Block_user (
+                            blocker_user_id UUID NOT NULL REFERENCES Human_user(user_id),
+                            blocked_user_id UUID NOT NULL REFERENCES Users(user_id),
+                            PRIMARY KEY (blocker_user_id, blocked_user_id)
+);
+
+CREATE TABLE Bot (
+                      user_id UUID PRIMARY KEY REFERENCES Users(user_id),
+                      users_counter INT DEFAULT 0
+);
+
+CREATE TABLE Bot_Commands (
+                              user_id UUID NOT NULL REFERENCES Users(user_id),
+                              cmd_text STRING(10) NOT NULL,
+                              PRIMARY KEY (user_id, cmd_text)
+);
+
+CREATE TABLE Story (
+                       user_id UUID NOT NULL REFERENCES Users(user_id),
+                       media_id UUID NOT NULL REFERENCES Media(media_id),
+                       PRIMARY KEY (user_id, media_id)
+);
+
+CREATE TABLE Send_Slicker (
+                              user_id UUID NOT NULL REFERENCES Users(user_id),
+                              sticker_id UUID NOT NULL,
+                              counter INT,
+                              PRIMARY KEY (user_id, sticker_id)
+);
+
+ALTER TABLE Send_Slicker ADD CONSTRAINT fk_sticker_id FOREIGN KEY (sticker_id) REFERENCES Sticker(sticker_id);
+ALTER TABLE Has_Slicker_Pack ADD CONSTRAINT fk_sticker_pack_id FOREIGN KEY (sticker_pack_id) REFERENCES Sticker_Pack(sticker_pack_id);
+
+
+CREATE TABLE Has_Slicker_Pack (
+                                  user_id UUID NOT NULL REFERENCES Users(user_id),
+                                  sticker_pack_id UUID NOT NULL,
+                                  PRIMARY KEY (user_id, sticker_pack_id)
+);
+
+CREATE TABLE Session (
+                         session_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                         auth_user_id UUID NOT NULL REFERENCES Users(user_id),
+                         auth_status STRING(2) NOT NULL DEFAULT '0',
+                         ip STRING(10) NOT NULL,
+                         location STRING(10) DEFAULT 'Unknown',
+                         create_time DATE NOT NULL DEFAULT now(),
+                         last_activity DATE NOT NULL DEFAULT now(),
+                         device STRING(10) DEFAULT 'Unknown',
+                         app_os STRING(10) DEFAULT 'Unknown',
+                         access_secret_call BIT(2) DEFAULT B'11'
+);
+
 
