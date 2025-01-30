3a041d3d202cc88d5c014dd9d8cf70cec8fb0df7
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2025-01-30 15:11:47 +0330

    add User, Media Tbale

--- rads_part.sql
+++ rads_part.sql
@@ -1,1 +1,29 @@
 -- abeds_part
+CREATE TABLE Users (
+                       user_id UUID PRIMARY KEY,
+                       username STRING(10) UNIQUE ,
+                       bio STRING(50) DEFAULT '',
+                       public_prof_id UUID
+);
+
+CREATE TABLE Media (
+                       media_id UUID PRIMARY KEY,
+                       path STRING UNIQUE NOT NULL,
+                       media_type STRING(10),
+                       file_size INT,
+                       uploader_id UUID NOT NULL
+);
+
+ALTER TABLE Users ADD CONSTRAINT fk_public_prof_id FOREIGN KEY (public_prof_id) REFERENCES Media(media_id);
+ALTER TABLE Media ADD CONSTRAINT fk_uploader_id FOREIGN KEY (uploader_id) REFERENCES Users(user_id);
+
+ALTER TABLE Users ALTER COLUMN user_id SET DEFAULT gen_random_uuid();
+ALTER TABLE Media ALTER COLUMN media_id SET DEFAULT gen_random_uuid();
+ALTER TABLE Users ALTER COLUMN username TYPE STRING(10);
+
+
+show tables;
+
+INSERT INTO Users (username, bio) VALUES ('john_doe', 'Software Engineer');
+
+
