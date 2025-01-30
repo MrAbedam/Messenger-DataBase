c959b057979941d3b839fa219ae7a07458d2c234
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2025-01-31 03:12:51 +0330

    fix Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -4,7 +4,7 @@ SELECT pc.title AS group_name, COUNT(m.message_id) AS message_count
 FROM groups g
 JOIN Public_Conversation pc ON g.conv_id = pc.conv_id
 JOIN Message m ON g.conv_id = m.conv_id
-WHERE m.send_time >= NOW() - INTERVAL '10 days'
+WHERE NOW() - m.send_time <=   INTERVAL '60 min'
 GROUP BY pc.title
 ORDER BY message_count DESC;
 
